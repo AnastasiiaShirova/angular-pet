6a7bc3f92b737c6854c38d91e02e5b6463193903
Anastasiia Shirova <anastasiyamironova711@gmail.com>
2024-05-03 15:54:52 +0300

    Added buttons + and - in card. Added function to compare product's in bag quantity with product quantity.

--- src/app/shared/services/bag.service.ts
+++ src/app/shared/services/bag.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import {Subject} from "rxjs";
 import {ProductInfo} from "../interfaces/product-card.interface";
 
 @Injectable({
@@ -8,12 +7,27 @@ import {ProductInfo} from "../interfaces/product-card.interface";
 export class BagService {
 
   constructor() {}
-  productsInBag = new Map();
+  productsInBag = new Map<string, { quantity: number; price: number }>();
+
   addProductsInBag(product: ProductInfo) {
-    if(!this.productsInBag.has(product.name)){
+    if(this.productsInBag.has(product.name) && this.productsInBag.get(product.name)!.quantity >= product.quantity) {
+      return;
+    }
+    if (!this.productsInBag.has(product.name)) {
       this.productsInBag.set(product.name, {quantity: 1, price: product.price})
     } else {
-      this.productsInBag.set(product.name, {quantity: this.productsInBag.get(product.name).quantity + 1, price: product.price})
+      this.productsInBag.set(product.name, {
+        quantity: this.productsInBag.get(product.name)!.quantity + 1,
+        price: product.price
+      })
+    }
+  }
+
+  deleteProductsFromBag(product: ProductInfo) {
+    if(this.productsInBag.get(product.name)?.quantity === 1) {
+      this.productsInBag.delete(product.name)
+    } else {
+      this.productsInBag.set(product.name, {quantity: this.productsInBag.get(product.name)!.quantity - 1, price: product.price})
     }
   }
 
